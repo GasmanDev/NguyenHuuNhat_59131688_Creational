1829df535460128fa03d85d6c9a3d8693951b152
Gasman <huunhat07@gmail.com>
2020-05-09 16:29:08 +0700

    Create class HoaDonHeader CTHD properties

---
+++ src/bt1/HoaDonHeader.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bt1;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Gasman
+ */
+public class HoaDonHeader {
+    String maHoaDon;
+    String tenKhachHang;
+    Date ngayBan;
+
+    public HoaDonHeader() {
+    }
+    public HoaDonHeader(String maHoaDon, String tenKhachHang, Date ngayBan) {
+        this.maHoaDon = maHoaDon;
+        this.tenKhachHang = tenKhachHang;
+        this.ngayBan = ngayBan;
+    }
+
+    public String getMaHoaDon() {
+        return maHoaDon;
+    }
+
+    public String getTenKhachHang() {
+        return tenKhachHang;
+    }
+
+    public Date getNgayBan() {
+        return ngayBan;
+    }
+
+    public void setMaHoaDon(String maHoaDon) {
+        this.maHoaDon = maHoaDon;
+    }
+
+    public void setTenKhachHang(String tenKhachHang) {
+        this.tenKhachHang = tenKhachHang;
+    }
+
+    public void setNgayBan(Date ngayBan) {
+        this.ngayBan = ngayBan;
+    }
+    
+    
+}
