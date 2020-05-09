9900b9bae5dc40b43041cd6759e181cfed91de5e
Gasman <huunhat07@gmail.com>
2020-05-09 16:33:10 +0700

    Class CTHD

--- src/bt1/CTHD.java
+++ src/bt1/CTHD.java
@@ -14,5 +14,47 @@ public class CTHD {
     int soLuong;
     double donGia;
     float chietKhau;
+
+    public CTHD() {
+    }
+
+    public CTHD(String sanPham, int soLuong, double donGia, float chietKhau) {
+        this.sanPham = sanPham;
+        this.soLuong = soLuong;
+        this.donGia = donGia;
+        this.chietKhau = chietKhau;
+    }
+
+    public String getSanPham() {
+        return sanPham;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public double getDonGia() {
+        return donGia;
+    }
+
+    public float getChietKhau() {
+        return chietKhau;
+    }
+
+    public void setSanPham(String sanPham) {
+        this.sanPham = sanPham;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public void setDonGia(double donGia) {
+        this.donGia = donGia;
+    }
+
+    public void setChietKhau(float chietKhau) {
+        this.chietKhau = chietKhau;
+    }
     
 }
